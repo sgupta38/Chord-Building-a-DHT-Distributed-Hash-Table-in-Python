377718758635d719c383309838df9a4b144a406f
Sonu Gupta <sonugupta4636@gmail.com>
2018-10-07 21:32:07 -0400

    Added helper functions to create file and hash the contents.

--- src/server.py
+++ src/server.py
@@ -7,8 +7,10 @@ import glob
 import sys
 sys.path.append('gen-py')
 sys.path.insert(0, glob.glob('/home/yaoliu/src_code/local/lib/lib/python2.7/site-packages')[0])
+from pathlib import Path
 
 from chord import FileStore
+import hashlib
 
 from chord.ttypes import SystemException, RFileMetadata, RFile, NodeID
 
@@ -17,15 +19,49 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
+####
+## HELPER Functions
+####
+
+def calculate256hash(data):
+    return hashlib.sha256(data.encode('utf8')).hexdigest()
+
+def createFile(rFile):
+#    try:
+        #check if file exits or not.
+        file = Path(rFile.meta.filename)
+        if file.is_file():
+            ## exists, so overwriting contents and incrementing version attribute.
+
+            print('File Already exists')
+            f = open(rFile.meta.filename, "w")
+            f.write(rFile.content)
+            rFile.meta.version = (int(rFile.meta.version) + 1)     ## Somewhere, need to store this in memory. What if another client connects, we have to keep record of last number.
+            rFile.meta.contentHash = calculate256hash(rFile.content)
+            print('incremented file version', rFile.meta.version)
+
+        else:
+            ## Doesn't exist, so creating new file.
+
+            f = open(rFile.meta.filename, "w")
+            f.write(rFile.content)
+            rFile.meta.version = 0
+            print("Successfully created file")
+
+        return rFile
+ #   except:
+        print('Error in createFile()')
+
 
 class FileStoreHandler:
 
     def writeFile(self, rFile):
         print('writeFile called()')
-        print('  FileName: ', rFile.meta.filename)
-        print('  Version: ', rFile.meta.version)
-        print('  Content Hash: ', rFile.meta.contentHash)
-        print('  Content : ', rFile.content)
+        rFile1 =  createFile(rFile)
+        print('  FileName: ', rFile1.meta.filename)
+        print('  Version: ', rFile1.meta.version)
+        print('  Content Hash: ', rFile1.meta.contentHash)
+        print('  Content : ', rFile1.content)
 
     def readFile(self, filename):
         print('readFile called()')
